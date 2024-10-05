5d41670cc6ec4e98be496bc5ea84e472fac31d94
KevinVincent016 <kevinvincentvl016@gmail.com>
2024-10-04 21:33:13 -0500

    Add db config, add modules, add first entity (users)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { PatientsModule } from './patients/patients.module';
+import { PetsModule } from './pets/pets.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { MedicalRecordsModule } from './medical_records/medical_records.module';
+import { ServicesModule } from './services/services.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      port: +process.env.DB_PORT,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    AuthModule, CommonModule, PatientsModule, PetsModule, AppointmentsModule, MedicalRecordsModule, ServicesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  name: string;
+
+  @Column('text', { unique: true })
+  email: string;
+
+  @Column('text', {select: false})
+  password: string;
+
+  @Column('text', {array:true, default:['owner']})
+  role: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updated_at: Date;
+}
