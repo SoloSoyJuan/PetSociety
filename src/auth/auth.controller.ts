7e770e7da86aed7cd3f6396a9f7a5f2ddb4edf18
Jeison Lasprilla <101517638+JeisonLasprilla@users.noreply.github.com>
2024-10-08 05:07:33 -0500

    feat(.controllers) use of role guards on controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -20,14 +20,14 @@ import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from './guards/roles.guard';
 import { Roles } from './decorators/roles.decorator';
 
-@UseGuards(RolesGuard)
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
   createUser(@Body() createUserDto: CreateUserDto) {
-    return this.authService.createUser(createUserDto);
+    // Por defecto es 'owner'
+    return this.authService.createUser({ ...createUserDto, role: 'owner' });
   }
 
   @Post('login')
@@ -35,36 +35,44 @@ export class AuthController {
     return this.authService.loginUser(loginUserDto);
   }
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtAuthGuard)
   @Post('logout')
   logout(@Res() res: Response) {
-    // Limpiar la cookie o token
-    res.clearCookie('token'); // Solo si usas cookies para el token
+    res.clearCookie('token');
     return res.status(200).json({ message: 'Logged out successfully' });
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Post('create-admin')
+  @Roles('admin')
+  createAdmin(@Body() createUserDto: CreateUserDto) {
+    return this.authService.createUser({ ...createUserDto, role: 'admin' });
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get()
   @Roles('admin')
   async getAllUsers() {
     return await this.authService.findAllUsers();
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get(':id')
+  @Roles('admin')
   async getUserById(@Param('id') id: number) {
     return await this.authService.findUserById(id);
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get('role/:role')
   @Roles('admin')
   async getUsersByRole(@Param('role') role: string) {
     return await this.authService.findUsersByRole(role);
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Patch(':id')
+  @Roles('admin')
   async updateUser(
     @Param('id') id: number,
     @Body() updateUserDto: UpdateUserDto,
@@ -72,7 +80,7 @@ export class AuthController {
     return await this.authService.updateUser(id, updateUserDto);
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Delete(':id')
   @Roles('admin')
   async deleteUser(@Param('id') id: number) {
