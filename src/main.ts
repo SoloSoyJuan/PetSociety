4eaacd6257d03cbfaf754739a157644f06059d82
KevinVincent016 <kevinvincentvl016@gmail.com>
2024-11-05 16:19:45 -0500

    Permisos para el FrontEnd

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  // Configuración de CORS
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',  // Tu frontend en desarrollo
+      'petsociety-production.up.railway.app'  // Tu frontend en producción
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  });
 }
 bootstrap();
