410c910c48b15e9db9c8a8cd5b03c51ffbe2c28b
Jeison Lasprilla <101517638+JeisonLasprilla@users.noreply.github.com>
2024-11-06 21:14:33 -0500

    fix(main.ts): CORS before app.listen

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-
-  // Configuración de CORS
+  
+  // Configuración de CORS ANTES de app.listen()
   app.enableCors({
     origin: [
       'http://localhost:3000',  // Tu frontend en desarrollo
-      'petsociety-production.up.railway.app'  // Tu frontend en producción
+      'https://petsociety-production.up.railway.app'  // Tu frontend en producción
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   });
+
+  // app.listen() debe ser lo último
+  await app.listen(3000);
 }
 bootstrap();
